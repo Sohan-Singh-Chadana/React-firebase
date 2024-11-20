a3178935dcfb50bbfce284a8486c0e4e64d42d7e
Sohan Singh <sohansinghchadana83@gmail.com>
2024-11-21 01:40:29 +0530

    demo project

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD62w-AaVNgmTPa0txJSIn8oM6Bae2e528",
-  authDomain: "fir-project-e14a1.firebaseapp.com",
-  projectId: "fir-project-e14a1",
-  storageBucket: "fir-project-e14a1.firebasestorage.app",
-  messagingSenderId: "349004101128",
-  appId: "1:349004101128:web:787d78d1bb566a8f2d83bd",
-  measurementId: "G-GLN9EKS0YQ",
-  databaseURL: "https://fir-project-e14a1-default-rtdb.firebaseio.com",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 };
 
 export const app = initializeApp(firebaseConfig);
